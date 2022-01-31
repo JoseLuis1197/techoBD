a967de34bc100fbeda37ecb4018f290fa8e394a6
Jose Proano Sipan <joseluisproanosipan@gmail.com>
2022-01-30 23:38:33 -0500

    feat: Adding new rows into video tables

--- insertData.sql
+++ insertData.sql
@@ -27,13 +27,13 @@ commit;
 
 delete from tbl_video;
 
-insert into tbl_video (sName,sAddress,sVideoType) values ('Módulo 01','https://bucket-techo-dev.s3.us-east-2.amazonaws.com/Video+M%C3%93DULO+1.mp4','V');
-insert into tbl_video (sName,sAddress,sVideoType) values ('Módulo 02 - Parte I','https://bucket-techo-dev.s3.us-east-2.amazonaws.com/Video+M%C3%93DULO+2+-+Parte+1.mp4','V');
-insert into tbl_video (sName,sAddress,sVideoType) values ('Módulo 02 - Parte II','https://bucket-techo-dev.s3.us-east-2.amazonaws.com/Video+M%C3%93DULO+2+-+Parte+2.mp4','V');
-insert into tbl_video (sName,sAddress,sVideoType) values ('Módulo 03 - Parte I','https://bucket-techo-dev.s3.us-east-2.amazonaws.com/Video+M%C3%93DULO+3+-+Parte+1.mp4','V');
-insert into tbl_video (sName,sAddress,sVideoType) values ('Módulo 03 - Parte II','https://bucket-techo-dev.s3.us-east-2.amazonaws.com/Video+M%C3%93DULO+3+-+Parte+2.mp4','V');
-insert into tbl_video (sName,sAddress,sVideoType) values ('Programa de organizaciones 1','https://bucket-techo-dev.s3.us-east-2.amazonaws.com/Video+PRESNTAC+1.mp4','O');
-insert into tbl_video (sName,sAddress,sVideoType) values ('Programa de organizaciones 2','https://bucket-techo-dev.s3.us-east-2.amazonaws.com/Video+PRESNTAC+2.mp4','O');
-insert into tbl_video (sName,sAddress,sVideoType) values ('Programa de organizaciones 3','https://bucket-techo-dev.s3.us-east-2.amazonaws.com/Video+PRESNTAC+3.mp4','O');
+insert into tbl_video (sName,sAddress,sVideoType,sDescription,sSpeaker,sTiming,sConditions) values ('Módulo 01','https://bucket-techo-dev.s3.us-east-2.amazonaws.com/Video+M%C3%93DULO+1.mp4','V','123','Juana','1 hora y 20 minutos','<ul><li>ejemplo1</li><li>Ejemplo2</li></ul>');
+insert into tbl_video (sName,sAddress,sVideoType) values ('Módulo 02 - Parte I','https://bucket-techo-dev.s3.us-east-2.amazonaws.com/Video+M%C3%93DULO+2+-+Parte+1.mp4','V','123','Juana','1 hora y 20 minutos','<ul><li>ejemplo1</li><li>Ejemplo2</li></ul>');
+insert into tbl_video (sName,sAddress,sVideoType) values ('Módulo 02 - Parte II','https://bucket-techo-dev.s3.us-east-2.amazonaws.com/Video+M%C3%93DULO+2+-+Parte+2.mp4','V','123','Juana','1 hora y 20 minutos','<ul><li>ejemplo1</li><li>Ejemplo2</li></ul>');
+insert into tbl_video (sName,sAddress,sVideoType) values ('Módulo 03 - Parte I','https://bucket-techo-dev.s3.us-east-2.amazonaws.com/Video+M%C3%93DULO+3+-+Parte+1.mp4','V','123','Juana','1 hora y 20 minutos','<ul><li>ejemplo1</li><li>Ejemplo2</li></ul>');
+insert into tbl_video (sName,sAddress,sVideoType) values ('Módulo 03 - Parte II','https://bucket-techo-dev.s3.us-east-2.amazonaws.com/Video+M%C3%93DULO+3+-+Parte+2.mp4','V','123','Juana','1 hora y 20 minutos','<ul><li>ejemplo1</li><li>Ejemplo2</li></ul>');
+insert into tbl_video (sName,sAddress,sVideoType) values ('Programa de organizaciones 1','https://bucket-techo-dev.s3.us-east-2.amazonaws.com/Video+PRESNTAC+1.mp4','O','123','Juana','1 hora y 20 minutos','<ul><li>ejemplo1</li><li>Ejemplo2</li></ul>');
+insert into tbl_video (sName,sAddress,sVideoType) values ('Programa de organizaciones 2','https://bucket-techo-dev.s3.us-east-2.amazonaws.com/Video+PRESNTAC+2.mp4','O','123','Juana','1 hora y 20 minutos','<ul><li>ejemplo1</li><li>Ejemplo2</li></ul>');
+insert into tbl_video (sName,sAddress,sVideoType) values ('Programa de organizaciones 3','https://bucket-techo-dev.s3.us-east-2.amazonaws.com/Video+PRESNTAC+3.mp4','O','123','Juana','1 hora y 20 minutos','<ul><li>ejemplo1</li><li>Ejemplo2</li></ul>');
 
 commit;
